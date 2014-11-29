c4d1857b3d395ad9c2ab9a742f110469c507ba8b
DBS <DBS@dbs10>
2014-11-29 16:12:44 -0200

    Tela Cantor finalizada.

--- src/main/java/br/com/dbs/java/mps/controller/CantorController.java
+++ src/main/java/br/com/dbs/java/mps/controller/CantorController.java
@@ -5,8 +5,10 @@ import br.com.dbs.java.mps.model.dao.CantorDao;
 import br.com.dbs.java.mps.model.dao.hibernate.CantorDaoHibernate;
 import br.com.dbs.java.mps.util.ImagemUtil;
 import br.com.dbs.java.mps.view.CantorFrame;
+import br.com.dbs.java.mps.view.table.CantorTableModel;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
@@ -16,6 +18,7 @@ public class CantorController {
     private CantorFrame view;
     private Cantor cantor;
     private CantorDao cantorDao;
+    private CantorTableModel cantorTableModel;
     
     public CantorController(CantorFrame view) {
         this.view = view;
@@ -36,6 +39,7 @@ public class CantorController {
             cantorDao.atualiza(cantor);
         }
         
+        preencheTabela();
         view.mostraMensagem("Salvo com sucesso!");
     }
     
@@ -64,7 +68,43 @@ public class CantorController {
         }
     }
     
-    
+    public void preencheTabela() {
+        List<Cantor> lista = cantorDao.lista();
+        cantorTableModel = new CantorTableModel(lista);
+        view.atualizaTabelaDeCantores(cantorTableModel);
+    }
+
+    public void preencheCampos(int linhaSelecionada) {
+        cantor = 
+                cantorTableModel.getCantor(linhaSelecionada);
+        
+        view.setNome(cantor.getNome());
+        view.setSobrenome(cantor.getSobrenome());
+        view.setFoto(ImagemUtil.
+                converteByteEmIcone(cantor.getFoto()));
+    }
+
+    public void exclui(int linhaSelecionada) {
+        cantor = cantorTableModel.getCantor(linhaSelecionada);
+        cantorDao.remove(cantor.getId());
+        preencheTabela();
+        view.mostraMensagem("Cantor excluído!");
+    }
+
+    public void pesquisa() {
+        final String nomeFiltro = view.getNomeFiltro();
+        
+        List<Cantor> cantores;
+        
+        if (nomeFiltro == null || nomeFiltro.isEmpty()) {
+            cantores = cantorDao.lista();
+        } else {
+            cantores = cantorDao.pesquisaPorNome(nomeFiltro);
+        }
+        
+        cantorTableModel = new CantorTableModel(cantores);
+        view.atualizaTabelaDeCantores(cantorTableModel);
+    }
     
     
     
--- src/main/java/br/com/dbs/java/mps/model/dao/CantorDao.java
+++ src/main/java/br/com/dbs/java/mps/model/dao/CantorDao.java
@@ -1,7 +1,11 @@
 package br.com.dbs.java.mps.model.dao;
 
 import br.com.dbs.java.mps.model.Cantor;
+import java.util.List;
 
-public interface CantorDao extends GenericDao<Cantor> {
+public interface CantorDao 
+    extends GenericDao<Cantor> {
+    
+    List<Cantor> pesquisaPorNome(String nome);
     
 }
